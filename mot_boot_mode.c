6ea65fa7935021a2cf5edd2738082862be7b2699
Alexander Khryukin <alexander@mezon.ru>
2011-07-06 15:13:33 +0400

    added conditions for tcmd.suspend or chargeonly

--- mot_boot_mode.c
+++ mot_boot_mode.c
@@ -21,6 +21,7 @@
 #define LOG_TAG "mot_boot_mode"
 #define PROPERTY_MOT_BOOT "tcmd.suspend"
 #define PROPERTY_ADB_BOOT "persist.service.adb.enable"
+#define POWER_REASON "0x00004000"
 
 static int ver_major = 0;
 static int ver_minor = 1;
@@ -35,17 +36,28 @@ int main(int argc, char **argv)
 	FILE *f;
 	f = fopen("/proc/bootinfo", "r");
 	char str[128];
+	char *d;
 	if (!f) {
 	
 		LOGE("Error at opening '%s'", f);
 		return NULL;
 	}
 
-	while(fgets(str, 128, f))
+	while(fgets(str, 128, f)){
 		LOGI("%s", str);
+	d = strstr(str, "POWERUPREASON");
+	if(d) {
+		d = strstr(str, ":") + 2;
+		LOGI("MOTO PWR_RSN : %s", d);
+	
+		if(!strncmp(d, POWER_REASON, 10)) 
+			property_set(PROPERTY_MOT_BOOT, "0");	
+			LOGI("Set tcmd.suspend to 0");
+		}
 	fclose(f);
-	property_set(PROPERTY_MOT_BOOT, "0");
-	LOGI("Set tcmd.suspend to 0");
+	}
+//	property_set(PROPERTY_MOT_BOOT, "0");
+//	LOGI("Set tcmd.suspend to 0");
 	property_set(PROPERTY_ADB_BOOT, "1");
 
 	LOGI("ADB at boot enabled");
