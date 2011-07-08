a4de0078a11c297090ce4961fba2c57248933aa9
Alexander Khryukin <alexander@mezon.ru>
2011-07-08 16:16:20 +0400

    Rewritten and version bump

--- mot_boot_mode.c
+++ mot_boot_mode.c
@@ -22,65 +22,65 @@
 #define PROPERTY_SYS_CHRGNLY "sys.chargeonly.mode"
 #define PU_REASON_SW_AP_RESET "0x00004000" //normal boot
 #define PU_REASON_CHARGER "0x00000100" //chargeonly.mode
-/*cid_recover_boot contains the flag to indicate whether phone should boot into recover mode or not.*/
 #define PU_REASON_CID_NORMAL "0x00"
 
-
-
 static int ver_major = 0;
-static int ver_minor = 2;
+static int ver_minor = 3;
 
-int main(int argc, char **argv)
-{
-	LOGI(":MOTO_PUPD: mot_boot_mode %d.%d", ver_major, ver_minor);
-	LOGI(":MOTO_PUPD: cid_recover_boot=0x00");
-
-//print content of /proc/bootinfo
-
-	FILE *f;
-	f = fopen("/proc/bootinfo", "r");
-	char str[128];
-	char *d;
-	if (!f) {
-		LOGE("Error at opening '%s'", f);
-		return NULL;
-	}
-	while(fgets(str, 128, f)){
-		LOGI("%s", str);
-	d = strstr(str, "POWERUPREASON");
-	if(d) {
-		d = strstr(str, ":") + 2;
-		LOGI("MOTO PWR_RSN : %s", d);
-	
-		if(!strncmp(d, PU_REASON_SW_AP_RESET, 10)) {
-			property_set(PROPERTY_MOT_BOOT, "0");	
-			LOGI("Normal boot");
-		}
-		if(!strncmp(d, PU_REASON_CHARGER, 10)) {
-			property_set(PROPERTY_SYS_CHRGNLY, "1");	
-			LOGI("Charge only mode enabled");
-		}
-		if(!strncmp(d, PU_REASON_CID_NORMAL, 4)) {
-		LOGI(":MOTO_PUPD: cid_recover_boot= %s", d);
-		}
-		}
-	fclose(f);
-	}
-//	property_set(PROPERTY_MOT_BOOT, "0");
-//	LOGI("Set tcmd.suspend to 0");
-	property_set(PROPERTY_ADB_BOOT, "1");
-
-	LOGI("ADB at boot enabled");
-	LOGD(":MOTO_PUPD: mot_boot_mode 12m: 0");
-
+static const char* enable_adb(void){
 	FILE *fp;
 	fp = fopen("/dev/usb_device_mode", "w");
-        if (!fp) {
-
-                LOGE("Error at opening '%s'", fp);
-                return NULL;
-        }
+	if (!fp) {
+		LOGE("Error at opening file", fp);
+		return NULL;
+	}
 	fprintf(fp, "msc_adb");
 	fclose(fp);
+	LOGI("ADB Enabled");
+	return NULL;
+}
+
+int parse(const char * haystack, const char * needle, const char * compare)
+	{
+	char *tmp = NULL;
+
+	tmp = strstr(haystack, needle);
+	if(tmp) {
+	tmp = strstr(tmp, ":") + 2;
+	if(!strncmp(tmp, compare, strlen(compare))) return tmp;
+	}
+
+return NULL;
+	} 
+
+int get_bootinfo(void){
+        FILE *f;
+	char *bootin;
+        char str[256];
+	f = fopen("/proc/bootinfo", "r");
+        if (!f) {
+                LOGE("Error at opening %s", f);
+                return NULL;
+        }
+	while(fgets(str, 128, f)){
+	if(bootin = parse(str, "POWERUPREASON", PU_REASON_SW_AP_RESET)) {
+	LOGI(":MOTO: pwr_rsn:%s", bootin);
+	property_set(PROPERTY_MOT_BOOT, "0");
+	LOGI(":MOTO: Normal boot");
+	}		
+	if(bootin = parse(str, "POWERUPREASON", PU_REASON_CHARGER)){ 
+	LOGI(":MOTO: pwr_rsn chargemode:%s", bootin);
+	property_set(PROPERTY_SYS_CHRGNLY, "1");
+	LOGI("Charge only mode enabled");
+	}
+	if(bootin = parse(str, "CID_RECOVER_BOOT", PU_REASON_CID_NORMAL)) {
+	LOGI(":MOTO: cid_recover_boot:%s", bootin);
+	}}
+	return NULL;
+}
+int main(int argc, char **argv){
+	LOGI(":MOTO_PUPD: mot_boot_mode %d.%d", ver_major, ver_minor);
+	enable_adb();
+	get_bootinfo();
 	return NULL;
 }
