345b1eae287cf64e0b39696d23bce0e807b354c4
Alexander Khryukin <alexander@mezon.ru>
2011-08-28 02:21:25 +0400

    merged with official sources

--- mot_boot_mode.c
+++ mot_boot_mode.c
@@ -1,35 +1,29 @@
-/* Reversed binary from motorola */
-#include <ctype.h>
-#include <errno.h>
-#include <getopt.h>
-#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h> 
 #include <string.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <sys/stat.h>
-#include <cutils/log.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <ctype.h>
+
 #include <cutils/properties.h>
-#include <cutils/sockets.h>
+#include <cutils/log.h>
 
-/* global definitions*/
-
-#define LOG_TAG "mot_boot_mode"
-#define PROPERTY_MOT_BOOT "tcmd.suspend"
-#define PROPERTY_ADB_BOOT "persist.service.adb.enable"
-#define PROPERTY_SYS_CHRGNLY "sys.chargeonly.mode"
-#define PU_REASON_SW_AP_RESET "0x00004000" //normal boot
-#define PU_REASON_PWR_KEY_PRESS "0x00000080" //pwr key pressed - normal boot
-#define PU_REASON_USB_CABLE "0x00000010" //pwr key pressed - normal boot
-#define PU_REASON_CHARGER "0x00000100" //chargeonly.mode
-#define PU_REASON_CID_NORMAL "0x00"
+#define MOTO_PU_REASON_CHARGE_ONLY    "0x00000100" 
+#define MOTO_CID_RECOVER_BOOT	      "0x01"
+#define MOTO_DATA_12M		      "1"
 
 static int ver_major = 0;
-static int ver_minor = 5;
+static int ver_minor = 6;
 
-static const char* enable_adb(void){
+/********************************************************************
+ * Check POWERUPREASON, decide phone powerup to charge only mode or not 
+ * Return value:
+ * Type: int
+ * 1: charge_only_mode
+ * 0: NOT charge_only_mode 
+ ********************************************************************/
+
+int enable_adb(void){
 	char value[PROPERTY_VALUE_MAX];
 	FILE *fp;
 	fp = fopen("/dev/usb_device_mode", "w");
@@ -39,75 +33,153 @@ static const char* enable_adb(void){
 	}
 	fprintf(fp, "msc_adb");
 	fclose(fp);
+	
 	if (property_get("persist.service.adb.enable", value, 0)) {
-		LOGI("adb service off");
+		LOGD("getprop return that adb disabled now");
 		property_set("persist.service.adb.enable", "1");
 	}
 	else {
 		property_get("persist.service.adb.enable", value, 1);
-		LOGI("adb service already enabled");
+		LOGD("adb service already enabled");
 	}
-	
-	LOGI("ADB Enabled");
+	LOGD("ADB Enabled");
 	return NULL;
-}
-
-int parse(const char * haystack, const char * needle, const char * compare)
-	{
-	char *tmp = NULL;
-
-	tmp = strstr(haystack, needle);
-	if(tmp) {
-	tmp = strstr(tmp, ":") + 2;
-	if(!strncmp(tmp, compare, strlen(compare))) return tmp;
 	}
 
-return NULL;
-	} 
+int boot_reason_charge_only(void)
+{
+    char data[1024], powerup_reason[32];
+    int fd, n;
+    char *x, *pwrup_rsn;
 
-int get_bootinfo(void){
-        FILE *f;
-	char *bootin = NULL;
-        char str[256];
-	f = fopen("/proc/bootinfo", "r");
-        if (!f) {
-                LOGE("Error at opening %s", f);
-                return NULL;
+
+    fd = open("/proc/bootinfo", O_RDONLY);
+    if (fd < 0) return 0;
+
+    n = read(fd, data, 1023);
+    close(fd);
+    if (n < 0) return 0;
+
+    data[n] = '\0';
+
+    memset(powerup_reason, 0, 32);
+
+    pwrup_rsn = strstr(data, "POWERUPREASON");
+    LOGD("MOTO : pwr_rsn = %s\n", pwrup_rsn);
+    if (pwrup_rsn) {
+        x = strstr(pwrup_rsn, ": ");
+        if (x) {
+            x += 2;
+            n = 0;
+            while (*x && !isspace(*x)) {
+                powerup_reason[n++] = *x;
+                x++;
+                if (n == 31) break;
+            }
+            powerup_reason[n] = '\0';
+            LOGD("MOTO_PUPD: powerup_reason=%s\n", powerup_reason);
         }
-	while(fgets(str, 128, f)){
-	
-	if(bootin = parse(str, "POWERUPREASON", PU_REASON_SW_AP_RESET)) {
-	LOGI(":MOTO: pwr_rsn:%s", bootin);
-	property_set(PROPERTY_MOT_BOOT, "0");
-	LOGI(":MOTO: Normal boot");
-	}		
-	
-	if(bootin = parse(str, "POWERUPREASON", PU_REASON_PWR_KEY_PRESS)) {
-	LOGI(":MOTO: pwr_rsn:%s", bootin);
-	property_set(PROPERTY_MOT_BOOT, "0");
-	LOGI(":MOTO: Power Key was pressed will start normal boot process");
-	}		
-	
-	if(bootin = parse(str, "POWERUPREASON", PU_REASON_CHARGER)){ 
-	LOGI(":MOTO: pwr_rsn chargemode:%s", bootin);
-	property_set(PROPERTY_SYS_CHRGNLY, "1");
-	LOGI("Charge only mode enabled");
-	}
-	
-	if(bootin = parse(str, "POWERUPREASON", PU_REASON_USB_CABLE)){ 
-	LOGI(":MOTO: pwr_rsn usb cable connected:%s", bootin);
-	property_set(PROPERTY_MOT_BOOT, "0");
-	LOGI("Usb cable connected now boot");
-	}
-	
-	if(bootin = parse(str, "CID_RECOVER_BOOT", PU_REASON_CID_NORMAL)) {
-	LOGI(":MOTO: cid_recover_boot:%s", bootin);
-	}}
-	return NULL;
+    }
+    if (!strncmp(powerup_reason, MOTO_PU_REASON_CHARGE_ONLY, 
+		 		(sizeof(MOTO_PU_REASON_CHARGE_ONLY)-1)))
+	return 1; 
+    else 
+	return 0; 
 }
-int main(int argc, char **argv){
-	LOGI(":MOTO_PUPD: mot_boot_mode %d.%d", ver_major, ver_minor);
-	enable_adb();
-	get_bootinfo();
-	return NULL;
+
+/********************************************************************
+ * Check CID_RECOVER_BOOT, decide phone powerup to recovery mode or not
+ * Return value:
+ * Type: int
+ * 1: Recovery mode
+ * 0: NOT recovery mode
+ * ******************************************************************/
+int check_cid_recover_boot(void)
+{
+    char data[1024], cid_recover_boot[32];
+    int fd, n;
+    char *x, *m_bmode;
+
+    memset(cid_recover_boot, 0, 32);
+    
+    fd = open("/proc/bootinfo", O_RDONLY);
+    if (fd < 0) return 0;
+   
+    n = read(fd, data, 1023);
+    close(fd);
+    if (n < 0) return 0;
+
+    data[n] = '\0';
+
+    m_bmode = strstr(data, "\nCID_RECOVER_BOOT");
+    if (m_bmode) {
+        x = strstr(m_bmode, ": ");
+        if (x) {
+            x += 2;
+            n = 0;
+            while (*x && !isspace(*x)) {
+                cid_recover_boot[n++] = *x;
+                x++;
+                if (n == 31) break;
+            }
+            cid_recover_boot[n] = '\0';
+            LOGD("MOTO_PUPD: cid_recover_boot=%s\n", cid_recover_boot);
+        }
+    }
+    
+    if (!strncmp(cid_recover_boot, MOTO_CID_RECOVER_BOOT, 
+		 		(sizeof(MOTO_CID_RECOVER_BOOT)-1)))
+	return 1; 
+    else 
+	return 0; 
+}
+
+/********************************************************************
+ * Check 12m file is set or not (tcmd suspend related) 
+ * return value:
+ * Type: int
+ * 1: 12m is set
+ * 0: 12m is not set
+ ********************************************************************/
+int check_data_12m(void)
+{
+    /*TODO: This 12m feature for TCMD need to be locked down*/
+    /*and implemented by TCMD team */
+    return 0;
+}
+
+/********************************************************************
+ * 1. Check cid_recover_boot, set property
+ * 2. Check boot reason is charge only mode or not, set property
+ * 3. Check 12m (tcmd suspend related), set property
+ * TODO: Is the priority/order right?
+ ********************************************************************/
+int main(int argc, char **argv)
+{    
+    LOGD("MOTO_PUPD: mot_boot_mode", ver_major, ver_minor);
+
+    if (check_cid_recover_boot()){
+
+        LOGD("MOTO_PUPD: check_cid_recover_boot: 1\n");
+        property_set("tcmd.cid.recover.boot", "1");
+        property_set("tcmd.suspend", "1");
+
+    }else if (boot_reason_charge_only()){
+
+    	LOGD("MOTO_PUPD: boot_reason_charge_only: 1\n");
+        property_set("sys.chargeonly.mode", "1");
+
+    }else if (check_data_12m()){
+
+        LOGD("MOTO_PUPD: mot_boot_mode 12m: 1\n");
+        property_set("tcmd.12m.test", "1");
+        property_set("tcmd.suspend", "1");
+
+    }else{
+
+       	LOGD("MOTO_PUPD: mot_boot_mode 12m: 0\n");
+       	property_set("tcmd.suspend", "0");
+    }
+
+    return 0;
 }
